eee51ae414975134548ab555f60a02457f7b1421
Thalles Cezar Rodrigues de Lima <tcrl@cin.ufpe.br>
2015-07-12 17:58:41 +0000

    SAR.ino adicionado os comandos de TV, falta descobrir como mandar os comandos para a TV Philips (nao existe Philips na biblioteca ):)

--- SAR.ino
+++ SAR.ino
@@ -1,6 +1,15 @@
-//#include <IRremote.h>
+#include <IRremote.h>
 #include <Servo.h>
 #include <BitVoicer11.h>
+
+// O LED IR deve estar conectado ao pino PWM 3
+
+#define VOL_UP    4
+#define VOL_DOWN  5
+#define CH_UP     6
+#define CH_DOWN   7
+#define POWER     8
+#define TV        10
 
 //Variáveis
 BitVoicerSerial bvSerial = BitVoicerSerial();
@@ -9,16 +18,16 @@ int valorComando = 0;
 int ValLedB = 0;
 int pinLedB = 2;
 int ValLedS = 0;
-int pinLedS = 3;
+int pinLedS = 3;  //TODO: Mudar, o IR usa o 3
 int ValLedQ = 0;
 int pinLedQ = 4;
 int pinMotor = 9;
 Servo motor;
+IRsend irsend;
 boolean ativarMotor = false;
+int estado = 0;
 
 boolean reverse = false;
-
-int estado = 1;
 
 void doMotor()
 {
@@ -42,12 +51,17 @@ void doMotor()
   ativarMotor = false;
 } 
 
+void sendPhilips(int comando){
+
+}
+
 //inicialização
 void setup() {
   Serial.begin(9600);
   pinMode(pinLedB, OUTPUT);
   pinMode(pinLedQ, OUTPUT);
   pinMode(pinLedS, OUTPUT);
+  pinMode(3, OUTPUT);       //Para o IR
   motor.attach(pinMotor);
 }
 void loop() {
@@ -65,20 +79,53 @@ void serialEvent()
   if (dataType == BV_INT)
   {
     valorComando = bvSerial.intData;
-    switch(valorComando)
-    {
-    case 1:
-      ValLedQ = !ValLedQ;
-      break;
-    case 2:
-      ValLedS = !ValLedS;
-      break;
-    case 3: 
-      ValLedB = !ValLedB;
-      break;
-    case 9:
-      ativarMotor = true;
-      break;
-    }
+    if(estado == 0){
+        switch(valorComando)
+        {
+        case 1:
+          ValLedQ = !ValLedQ;
+          break;
+        case 2:
+          ValLedS = !ValLedS;
+          break;
+        case 3: 
+          ValLedB = !ValLedB;
+          break;
+        case 9:
+          ativarMotor = true;
+          break;
+         case TV:
+          Serial.println("Comecou TV");
+          estado = 1;
+          break;
+        }
+      }else{
+        switch(valorComando)
+        {
+        case VOL_UP:
+          Serial.println("Volume UP");
+          break;
+        case VOL_DOWN:
+          Serial.println("Volume DOWN");      
+          break;
+        case CH_UP: 
+          Serial.println("Channel UP");    
+          break;
+        case CH_DOWN:
+          Serial.println("Channel Down");
+          break;
+        case POWER:
+          /*for (int i = 0; i < 100; i++){
+              irsend.sendSony(0xa90, 12); // Sony TV power code
+              delay(40);
+          }*/
+          Serial.println("Power");
+          break;
+        case TV:        
+          Serial.println("Cabou TV");
+          estado = 0;
+        break;
+        }
+      }
    }  
 }
